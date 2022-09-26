8a333ba3e16bb3a2c10937dafd22a2ed5523b106
Cheesetunafish <1614312228@qq.com>
2022-09-26 14:19:42 +0800

    demo

---
+++ wechat的副本/wechat/New Group/CollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.h
+//  wechat
+//
+//  Created by 小艾同学 on 2022/9/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ wechat的副本/wechat/New Group/CollectionViewCell.m
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.m
+//  wechat
+//
+//  Created by 小艾同学 on 2022/9/26.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
